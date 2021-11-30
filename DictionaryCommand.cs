2d221e48dc2a813adafbaee460f065849300f382
TiberSe <82593491+TiberSe@users.noreply.github.com>
2021-12-01 06:03:00 +0900

    Change Command Logic
    from if, to switch case

--- DictionaryCommand.cs
+++ DictionaryCommand.cs
@@ -9,7 +9,6 @@ using System.Xml.Linq;
 
 namespace Septim.DictPlugin {
     public class DictionaryCommand : IPlugin {
-
         public string Name { get; } = "Septim.DictPlugin.DictionaryCommand";
         public string HelpText { get; } =  "Tiber's Dictionary Opeartion Commands"+"```$learn key value :keyの読みをvalueとして登録 \n $forget key :keyの読みを消去```";
         Nursery.Plugins.Type IPlugin.Type => Nursery.Plugins.Type.Command;
@@ -18,52 +17,55 @@ namespace Septim.DictPlugin {
 
         public bool Execute(IBot bot, IMessage message) {
             string[] args = message.Original.Content.Split(' ');
-            if (args[0].StartsWith("$learn")) {
-                if (args.Length == 3) {
-                    AddDictFile(args[1], args[2]);
-                    bot.SendMessageAsync(message.Original.Channel, $"Learned:{args[1]} / {args[2]}", true);
+            switch (args[0]){
+                case "$learn":
+                    if (args.Length == 3) {
+                        AddDictFile(args[1], args[2]);
+                        bot.SendMessageAsync(message.Original.Channel, $"Learned:{args[1]} / {args[2]}", true);
+                        message.Content = "";
+                        message.AppliedPlugins.Add(this.Name);
+                        message.Terminated = true;
+                        return true;
+                    } else {
+                        bot.SendMessageAsync(message.Original.Channel, "引数が過剰もしくは不足しています。", true);
+                        message.Content = "";
+                        message.AppliedPlugins.Add(this.Name);
+                        message.Terminated = true;
+                        return true;
+                    }
+                case "$forget":
+                    if (args.Length == 2) {
+                        RemoveFromDict(args[1]);
+                        bot.SendMessageAsync(message.Original.Channel, $"Removed:{args[1]}", true);
+                        message.Content = "";
+                        message.AppliedPlugins.Add(this.Name);
+                        message.Terminated = true;
+                        return true;
+                    } else {
+                        bot.SendMessageAsync(message.Original.Channel, "引数が過剰もしくは不足しています。", true);
+                        message.Content = "";
+                        message.AppliedPlugins.Add(this.Name);
+                        message.Terminated = true;
+                        return true;
+                    }
+                case "$enableDictionary":
+                    Global.status = true;
+                    bot.SendMessageAsync(message.Original.Channel, "辞書機能を有効にしました。", true);
                     message.Content = "";
                     message.AppliedPlugins.Add(this.Name);
                     message.Terminated = true;
                     return true;
-                } else {
-                    bot.SendMessageAsync(message.Original.Channel, "引数が過剰もしくは不足しています。", true);
+                case "$unableDictionary":
+                    Global.status = false;
+                    bot.SendMessageAsync(message.Original.Channel, "辞書機能を無効にしました。", true);
                     message.Content = "";
                     message.AppliedPlugins.Add(this.Name);
                     message.Terminated = true;
                     return true;
-                }
-            } else if (args[0].StartsWith("$forget")){
-                if (args.Length == 2){
-                    RemoveFromDict(args[1]);
-                    bot.SendMessageAsync(message.Original.Channel, $"Removed:{args[1]}", true);
-                    message.Content = "";
-                    message.AppliedPlugins.Add(this.Name);
-                    message.Terminated = true;
-                } else {
-                    bot.SendMessageAsync(message.Original.Channel, "引数が過剰もしくは不足しています。", true);
-                    message.Content = "";
-                    message.AppliedPlugins.Add(this.Name);
-                    message.Terminated = true;
-                    return true;
-                }
-            } else if (args[0].StartsWith("$enableDictionary")) {
-                Global.status = true;
-                bot.SendMessageAsync(message.Original.Channel, "辞書機能を有効にしました。", true);
-                message.Content = "";
-                message.AppliedPlugins.Add(this.Name);
-                message.Terminated = true;
-
-            } else if (args[0].StartsWith("$unableDictionary")) {
-                Global.status = false;
-                bot.SendMessageAsync(message.Original.Channel, "辞書機能を無効にしました。", true);
-                message.Content = "";
-                message.AppliedPlugins.Add(this.Name);
-                message.Terminated = true;
-            }
-                return false;
+                default:
+                    return false;
+            }            
         }
-
         public void AddDictFile(string key, string value) {
             Global.Dict.Add(key, value);
             XDocument xmlDic = XDocument.Load("./dictionary.xml");
